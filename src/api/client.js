274f9ecf2bba4e3ae5ce4f98afbc00a1401ad5ab
furqonfatahillah <furqonfatahillah999@gmail.com>
2026-09-09 04:05:16 +0800

    update port

--- src/api/client.js
+++ src/api/client.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const apiHost = typeof window !== 'undefined' ? window.location.hostname : 'localhost';
 
 const api = axios.create({
-  baseURL: `http://${apiHost}:8000/api`,
+  baseURL: `http://${apiHost}/api`,
   headers: {
     'Content-Type': 'application/json',
     'Accept': 'application/json',
